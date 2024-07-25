17facce63a4ef7de30170a24221127ecd2b997ac
Natthaphong <natthaphong.m@nextsoft.me>
2024-07-25 15:03:22 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'interview_quiz_natthaphong';
+
+  //A ----------------------------------------------------------------
+  numbersA = [5, 2, 44, 12, 34];
+  getExampleA(numbers: number[]): number {
+    let maxValue = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+      if (numbers[i] > maxValue) {
+        maxValue = numbers[i];
+      }
+    }
+    return maxValue;
+  }
 }
+
